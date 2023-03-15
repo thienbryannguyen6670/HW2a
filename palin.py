ca8c8ff0b9db05f81ca53030ec3852821242ab45
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-03-15 15:30:31 -0700

    Update palin.py

--- palin.py
+++ palin.py
@@ -1,10 +1,10 @@
 def isPalindrome(my_str):
    if my_str == my_str[::-1]:
-      print("True")
-      else:
-        print("False")
+    print("True")
+   else:
+    print("False")
         
 my_list = [1,2,"Espresso","Madeline",2,1]
 
 my_list = ' '.join([str(elem) for elem in my_list])
-check_palindrome_list(my_list)
+isPalindrome(my_list)
