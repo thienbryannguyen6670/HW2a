722544fe64d47006f6e3fc92edc9d0a4323d30e9
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-03-17 12:28:13 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -18,13 +18,13 @@ def cacti_number(plot):
                 if j < cols-1 and plot[i][j+1] == 1:
                     adjacent_empty = True
                 if i > 0 and j > 0 and plot[i-1][j-1] == 1:
-                    adjacent_empty = True
+                    adjacent_empty = False
                 if i > 0 and j < cols-1 and plot[i-1][j+1] == 1:
-                    adjacent_empty = True
+                    adjacent_empty = False
                 if i < rows-1 and j > 0 and plot[i+1][j-1] == 1:
-                    adjacent_empty = True
+                    adjacent_empty = False
                 if i < rows-1 and j < cols-1 and plot[i+1][j+1] == 1:
-                    adjacent_empty = True
+                    adjacent_empty = False
                 if adjacent_empty:
                     count += 1
                 print(plot[i][j]) 
