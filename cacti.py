4185a531da1bb30f5d00a5ceff72ed29b459b538
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-03-17 10:17:11 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -1,27 +1,30 @@
-def cacti_number(func):
-    def wrapper(arr):
-        count = 0
-        for i in range(len(arr)):
-            for j in range(len(arr[i])):
-                if arr[i][j] == 0:
-                    if (i == 0 or arr[i-1][j] == 0) and \
-                       (i == len(arr)-1 or arr[i+1][j] == 0) and \
-                       (j == 0 or arr[i][j-1] == 0) and \
-                       (j == len(arr[i])-1 or arr[i][j+1] == 0):
-                        count += 1
-        return count
-    return wrapper
-
-@cacti_number
-def test_cacti_number(arr):
-    pass
-
-test_arr = [
-    [0, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-]
-
-print(test_cacti_number(test_arr))
-
+def cacti_number(plot):
+    # Get the dimensions of the plot
+    rows = len(plot)
+    cols = len(plot[0])
+    # Count the number of cacti that can still be planted
+    count = 0
+    for i in range(rows):
+        for j in range(cols):
+            if plot[i][j] == 0:
+                # Check if the adjacent blocks are empty
+                adjacent_empty = True
+                if i > 0 and plot[i-1][j] == 1:
+                    adjacent_empty = False
+                if j > 0 and plot[i][j-1] == 1:
+                    adjacent_empty = False
+                if i < rows-1 and plot[i+1][j] == 1:
+                    adjacent_empty = False
+                if j < cols-1 and plot[i][j+1] == 1:
+                    adjacent_empty = False
+                if i > 0 and j > 0 and plot[i-1][j-1] == 1:
+                    adjacent_empty = False
+                if i > 0 and j < cols-1 and plot[i-1][j+1] == 1:
+                    adjacent_empty = False
+                if i < rows-1 and j > 0 and plot[i+1][j-1] == 1:
+                    adjacent_empty = False
+                if i < rows-1 and j < cols-1 and plot[i+1][j+1] == 1:
+                    adjacent_empty = False
+                if adjacent_empty:
+                    count += 1
+    return count
