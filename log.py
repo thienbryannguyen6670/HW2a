bc5290dfaac17ec6fbb45c5974c3a7eb2ea5b86d
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-03-16 22:26:07 -0700

    Update log.py

--- log.py
+++ log.py
@@ -1,8 +1,7 @@
 import time
 
 def timestamp(func):
-  def wrapper(*args, **kwargs):
-    print(f"[{time.ctime()}]")
-    result = func(*args, **kwargs)
-    print(f"[{time.ctime()}]")
-    return wrapper
+  def printDate():
+    print(time.ctime())
+    func()
+  return printDate
