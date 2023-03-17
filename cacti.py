388af66fec0d3a8bc617a46d4ab9c96d3f83d4f7
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-03-16 22:23:52 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -1,18 +1,60 @@
+def count_cacti(plot):
+    count = 0
+    rows, cols = len(plot), len(plot[0])
+    for i in range(rows):
+        for j in range(cols):
+                if i > 0 and plot[i-1][j] == 1:
+                    adjacent = True
+                elif i < rows-1 and plot[i+1][j] == 1:
+                    adjacent = True
+                elif j > 0 and plot[i][j-1] == 1:
+                    adjacent = True
+                elif j < cols-1 and plot[i][j+1] == 1:
+                    adjacent = True
+                # Check for adjacent cacti diagonally
+                elif i > 0 and j > 0 and plot[i-1][j-1] == 1:
+                    adjacent = True
+                elif i > 0 and j < cols-1 and plot[i-1][j+1] == 1:
+                    adjacent = True
+                elif i < rows-1 and j > 0 and plot[i+1][j-1] == 1:
+                    adjacent = True
+                elif i < rows-1 and j < cols-1 and plot[i+1][j+1] == 1:
+                    adjacent = True
+                if not adjacent:
+                    count += 1
+    return count
 def cacti_number(func):
-    def wrapper(arr):
-        rows, cols = len(arr), len(arr[0])
-        count = 0
+    def wrapper(*args, **kwargs):
+        plot = args[0]
+        rows, cols = len(plot), len(plot[0])
+        max_cacti = 0
         for i in range(rows):
             for j in range(cols):
-                if arr[i][j] == 0:
-                    # Check all adjacent and diagonal positions for existing cacti
-                    adj_cacti = 0
-                    for r in range(max(0, i-1), min(rows, i+2)):
-                        for c in range(max(0, j-1), min(cols, j+2)):
-                            if arr[r][c] == 1:
-                                adj_cacti += 1
-                                break
-                    if adj_cacti == 0:
-                        count += 1
-        return count
-    return wrapper
+                if plot[i][j] == 0:
+                    adjacent = False
+                    # Check for adjacent cacti horizontally and vertically
+                    if i > 0 and plot[i-1][j] == 1:
+                        adjacent = True
+                    elif i < rows-1 and plot[i+1][j] == 1:
+                        adjacent = True
+                    elif j > 0 and plot[i][j-1] == 1:
+                        adjacent = True
+                    elif j < cols-1 and plot[i][j+1] == 1:
+                        adjacent = True
+                    # Check for adjacent cacti diagonally
+                    elif i > 0 and j > 0 and plot[i-1][j-1] == 1:
+                        adjacent = True
+                    elif i > 0 and j < cols-1 and plot[i-1][j+1] == 1:
+                        adjacent = True
+                    elif i < rows-1 and j > 0 and plot[i+1][j-1] == 1:
+                        adjacent = True
+                    elif i < rows-1 and j < cols-1 and plot[i+1][j+1] == 1:
+                        adjacent = True
+                    if not adjacent:
+                        test_plot = [row[:] for row in plot]
+                    test_plot[i][j] = 1
+                    cacti_count = count_cacti(test_plot)
+                    if cacti_count > max_cacti:
+                        max_cacti = cacti_count
+            return func(*args, **kwargs), max_cacti
+        return wrapper
