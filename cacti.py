dff32a2d9126a2129f4a6568831a168951d7219f
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-03-17 12:27:24 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -8,7 +8,7 @@ def cacti_number(plot):
         for j in range(cols):
             if plot[i][j] == 0:
                 # Check if the adjacent blocks are empty
-                adjacent_empty = True
+                adjacent_empty = False
                 if i > 0 and plot[i-1][j] == 1:
                     adjacent_empty = True
                 if j > 0 and plot[i][j-1] == 1:
