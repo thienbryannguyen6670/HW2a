e279d9810c21210282a3b4b2a3aa610bc31c7e20
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-03-17 09:44:44 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -1,60 +1,27 @@
-def count_cacti(plot):
+def cacti_number(plot):
+    rows = len(plot)
+    cols = len(plot[0])
     count = 0
-    rows, cols = len(plot), len(plot[0])
     for i in range(rows):
         for j in range(cols):
+            if plot[i][j] == 0:
+                adjacent_empty = True
                 if i > 0 and plot[i-1][j] == 1:
-                    adjacent = True
-                elif i < rows-1 and plot[i+1][j] == 1:
-                    adjacent = True
-                elif j > 0 and plot[i][j-1] == 1:
-                    adjacent = True
-                elif j < cols-1 and plot[i][j+1] == 1:
-                    adjacent = True
-                # Check for adjacent cacti diagonally
-                elif i > 0 and j > 0 and plot[i-1][j-1] == 1:
-                    adjacent = True
-                elif i > 0 and j < cols-1 and plot[i-1][j+1] == 1:
-                    adjacent = True
-                elif i < rows-1 and j > 0 and plot[i+1][j-1] == 1:
-                    adjacent = True
-                elif i < rows-1 and j < cols-1 and plot[i+1][j+1] == 1:
-                    adjacent = True
-                if not adjacent:
+                    adjacent_empty = False
+                if j > 0 and plot[i][j-1] == 1:
+                    adjacent_empty = False
+                if i < rows-1 and plot[i+1][j] == 1:
+                    adjacent_empty = False
+                if j < cols-1 and plot[i][j+1] == 1:
+                    adjacent_empty = False
+                if i > 0 and j > 0 and plot[i-1][j-1] == 1:
+                    adjacent_empty = False
+                if i > 0 and j < cols-1 and plot[i-1][j+1] == 1:
+                    adjacent_empty = False
+                if i < rows-1 and j > 0 and plot[i+1][j-1] == 1:
+                    adjacent_empty = False
+                if i < rows-1 and j < cols-1 and plot[i+1][j+1] == 1:
+                    adjacent_empty = False
+                if adjacent_empty:
                     count += 1
     return count
-def cacti_number(func):
-    def wrapper(*args, **kwargs):
-        plot = args[0]
-        rows, cols = len(plot), len(plot[0])
-        max_cacti = 0
-        for i in range(rows):
-            for j in range(cols):
-                if plot[i][j] == 0:
-                    adjacent = False
-                    # Check for adjacent cacti horizontally and vertically
-                    if i > 0 and plot[i-1][j] == 1:
-                        adjacent = True
-                    elif i < rows-1 and plot[i+1][j] == 1:
-                        adjacent = True
-                    elif j > 0 and plot[i][j-1] == 1:
-                        adjacent = True
-                    elif j < cols-1 and plot[i][j+1] == 1:
-                        adjacent = True
-                    # Check for adjacent cacti diagonally
-                    elif i > 0 and j > 0 and plot[i-1][j-1] == 1:
-                        adjacent = True
-                    elif i > 0 and j < cols-1 and plot[i-1][j+1] == 1:
-                        adjacent = True
-                    elif i < rows-1 and j > 0 and plot[i+1][j-1] == 1:
-                        adjacent = True
-                    elif i < rows-1 and j < cols-1 and plot[i+1][j+1] == 1:
-                        adjacent = True
-                    if not adjacent:
-                        test_plot = [row[:] for row in plot]
-                    test_plot[i][j] = 1
-                    cacti_count = count_cacti(test_plot)
-                    if cacti_count > max_cacti:
-                        max_cacti = cacti_count
-            return func(*args, **kwargs), max_cacti
-        return wrapper
