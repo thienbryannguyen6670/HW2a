6504f250d823c833b00541ef5581d0fe7f1aa9b2
thienbryannguyen6670 <125083690+thienbryannguyen6670@users.noreply.github.com>
2023-03-20 22:13:17 -0700

    Update cacti.py

--- cacti.py
+++ cacti.py
@@ -8,9 +8,9 @@ def cacti_number(func):
         for j in cols:
             if(func[i][j] != 1):
                 if((i -1 >= 0 and func[i-1][j] == 0) or i -1 < 0):
-                    if((j-1 >= 0 and func[i][j-1]==0) or j-1 < 0):
+                    if((j-1 >= 0 and func[i][j-1] == 0) or j-1 < 0):
                         if((j+1 < len(func[0]) and func[i][j+1] == 0) or j+1 >= len(func[0])):
-                            if((i+1<len(func[0]) and func[i+1][j] == 0) or i+1  >= len(func[0])):
+                            if((i+1 < len(func) and func[i+1][j] == 0) or i+1  >= len(func)):
                                 func[i][j] = 1
                                 num += 1
     return num
